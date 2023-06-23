5a85b57afe38a54608f75379cf62ac39e069961d
MatheusRodriguesBezerra <matheusrodbez@gmail.com>
2023-06-23 14:28:52 -0300

    folha 4 feita

--- folha4/index.hs
+++ folha4/index.hs
@@ -1,70 +1,78 @@
--- questao 1
-algarismosRev :: Int -> [Int]
-algarismosRev 0 = []
-algarismosRev n = [n `mod` 10] ++ algarismosRev (n`div` 10)
-
+-- 4.1
 algarismos :: Int -> [Int]
-algarismos n = reverse (algarismosRev n)
+algarismos n = reverse (algarismosAux n)
 
+algarismosAux :: Int -> [Int]
+algarismosAux n
+    | n < 10 = [n]
+    | otherwise = [n `mod` 10] ++ algarismosAux (n `div` 10)
 
--- questao 2
-toBitsRev :: Int -> [Int]
-toBitsRev 0 = []
-toBitsRev n = [n `mod` 2] ++ toBitsRev (n`div` 2)
-
+-- 4.2
 toBits :: Int -> [Int]
-toBits n = reverse (toBitsRev n)
+toBits n = reverse (toBitsAux n)
 
+toBitsAux :: Int -> [Int]
+toBitsAux n
+    | n < 2 = [n]
+    | otherwise = [n `mod` 2] ++ toBitsAux (n `div` 2)
 
--- questao 3
+-- 4.3
 fromBits :: [Int] -> Int
 fromBits [] = 0
-fromBits (x:xs) = x * 2^(length xs) + fromBits xs
+fromBits (x:xs) = x*(2^(length (xs))) + fromBits xs
 
-
--- questao 4
+-- 4.4 
 mdc :: Integer -> Integer -> Integer
-mdc a b 
-    | b == 0 = a
-    | otherwise = mdc b (a `mod` b)
+mdc a 0 = a
+mdc a b = mdc b (a `mod` b)
 
 
--- questao 5
-insert' :: Ord a => a -> [a] -> [a]
-insert' n [] = [n]
-insert' n (x:xs) 
-    | n < x = [n] ++ x:xs
-    | otherwise = [x] ++ insert' n xs
+-- 4.5a)
+myinsert :: Ord a => a -> [a] -> [a]
+myinsert n [] = [n]
+myinsert n (x:xs)
+    | n <= x = [n, x] ++ xs
+    | otherwise = [x] ++ myinsert n xs
 
-isort' :: Ord a => [a] -> [a] 
-isort' [] = []
-isort' (x:xs) = insert' x (isort' xs)
+-- 4.5b)
+mysort :: Ord a => [a] -> [a]
+mysort [] = []
+mysort (x:xs) = myinsert x (mysort xs)
 
 
--- questao 6
-minimum' :: Ord a => [a] -> a
-minimum' xs = head (isort' xs)
+-- 4.6a)
+myminimum :: Ord a => [a] -> a
+myminimum (x:xs) = minimumAux x l
+    where l = [x] ++ xs
 
-delete' :: Eq a => a -> [a] -> [a]
-delete' _ [] = []
-delete' n (x:xs) 
+minimumAux :: Ord a => a -> [a] -> a
+minimumAux n [] = n
+minimumAux n (x:xs)
+    | x < n = minimumAux x xs
+    | otherwise = minimumAux n xs
+
+-- 4.6b)
+delete :: Eq a => a -> [a] -> [a]
+delete n [] = []
+delete n (x:xs) 
     | n == x = xs
-    | otherwise = [x] ++ delete' n xs
+    | otherwise = [x] ++ delete n xs
 
-ssort' :: Ord a => [a] -> [a]
-ssort' [] = []
-ssort' xs = [minimum' xs] ++ ssort' (delete' (minimum' xs) xs) 
+-- 4.6c)
+ssort :: Ord a => [a] -> [a]
+ssort [] = []
+ssort xs = [myminimum xs] ++ ssort (delete (myminimum xs) xs)
 
+-- 4.7a)
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+    | y < x = [y] ++ merge (x:xs) ys
+    | x < y = [x] ++ merge xs (y:ys)
+    | otherwise = [x,y] ++ merge xs ys
 
--- questao 7
-merge :: Ord a => [a] -> [a] -> [a] 
-merge xs ys = ssort' (xs++ys) 
-
-
--- questao 8 
-addPoly :: [Int] -> [Int] -> [Int]
-addPoly [] [] = []
-addPoly xs [] = xs
-addPoly [] ys = ys
-addPoly (x:xs) (y:ys) = [x+y] ++ addPoly xs ys
-
+-- 4.7b)
+-- msort :: Ord a => [a] -> [a]
+-- msort [] = []
+-- msort [x] = [x]
